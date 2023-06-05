dd1513eb87a9d893b40853cce0a740cba03641f9
Cynthia <120834111+cyntlopez@users.noreply.github.com>
2023-06-04 21:08:49 -0700

    PlayScreen.java

--- src/PlayScreen.java
+++ src/PlayScreen.java
@@ -4,7 +4,10 @@ import java.awt.*;
 import java.awt.image.*;
 import java.util.Locale;
 
+//play screen is extended from game panel
 public class PlayScreen extends GamePanel{
+    
+    //variables declared for gameplay
     private final JPanel animationPanel = new JPanel();
     private int wrongAnswers = 0;
     private int problemIndex = 0;
@@ -14,18 +17,20 @@ public class PlayScreen extends GamePanel{
     private final JTextArea problemField = new JTextArea();
     private final JButton guess = new JButton("Enter");
 
+    //number of snowflakes
     private final SnowFlake[] snowflakes = new SnowFlake[15];
 
+    //constructor takes difficulty chosen and JFrame object
     public PlayScreen(String difficulty, JFrame frame ) {
         setLayout(new BorderLayout());
         setFrame(frame);
         setSize(getWidth(), getHeight());
         setBackground(Color.LIGHT_GRAY);
+        
+        //problems generated based off difficulty
         generateProblems(difficulty);
 
-
-
-
+        //set the location and size of answer box and problem field
         final int elementYLocation = 405;
         final int elementHeight = 40;
         final int spaceForElements = 100;
@@ -35,10 +40,11 @@ public class PlayScreen extends GamePanel{
         answerBox.setLocation(answerBoxLocation);
         answerBox.setSize(answerBoxSize);
 
-        //problem text field
+        //problem text field set up
         final int problemFieldCenter = 125;
         problemField.setBounds(problemFieldCenter - problems[problemIndex].getProblemSize()/2, elementYLocation + elementHeight / 4,
                 problems[problemIndex].getProblemSize(), elementHeight);
+        //field properties
         problemField.setEditable(false);
         problemField.setWrapStyleWord(true);
         problemField.setLineWrap(true);
@@ -47,21 +53,26 @@ public class PlayScreen extends GamePanel{
         problemField.setText(problems[problemIndex].getQuestion() + " = ?");
         problemField.setBackground(Color.LIGHT_GRAY);
 
-
+        //guess button configuration
         guess.setLocation(375, elementYLocation);
         guess.setSize(80, elementHeight);
+        //action listener added to guess button
         guess.addActionListener(e -> {
-            String userAnswer = answerBox.getText();
+            String userAnswer = answerBox.getText(); //answer received from field
             if(userAnswer.isBlank()) {
+                //show dialog box propmting user to enter a number
                 JOptionPane.showMessageDialog(this, "Enter a number to submit your answer!");
             }
-            else if(userAnswer.equalsIgnoreCase("AGAIN")) {
-                getFrame().remove(this);
-                getFrame().add(new HomeScreen(getFrame()));
+            
+            // user enters again to start game over
+            else if(userAnswer.equalsIgnoreCase("AGAIN")) { 
+                getFrame().remove(this); 
+                getFrame().add(new HomeScreen(getFrame())); 
                 getFrame().revalidate();
                 getFrame().repaint();
 
             }
+            //if the users answers all the questions correctly, the win screen appears
             else if (userAnswer.equals(String.valueOf((int)(problems[problemIndex].getAnswer())))) {
                 if(problemIndex == problems.length - 1) {
                     getFrame().remove(this);
@@ -69,14 +80,15 @@ public class PlayScreen extends GamePanel{
                     getFrame().revalidate();
                     getFrame().repaint();
                 }
-                else{
+                else{ //moves to next problem
                     problemIndex++;
+                    //set bounds and text for problem
                     problemField.setBounds(problemFieldCenter - problems[problemIndex].getProblemSize() / 2,
                             elementYLocation + elementHeight /4, problems[problemIndex].getProblemSize(), elementHeight);
                     problemField.setText(problems[problemIndex].getQuestion() + " = ?");
                 }
             }
-            else {
+            else { //if all incorrect attempts are maxed, lose screen appears
                 if (wrongAnswers == images.length - 1) {
                     getFrame().remove(this);
                     getFrame().add(new EndScreen("lose", getFrame()));
@@ -84,40 +96,40 @@ public class PlayScreen extends GamePanel{
                     getFrame().repaint();
                 }
                 else{
-                    //animationPanel.remove(images[wrongAnswers]);
+     
                     wrongAnswers++;
                     animationPanel.removeAll();
                     animationPanel.add(images[wrongAnswers]);
-                    for(SnowFlake s: snowflakes) {
+                    for(SnowFlake s: snowflakes) { //snowflakes are added to the animation panel
                         animationPanel.add(s);
                     }
                     animationPanel.revalidate();
                     animationPanel.repaint();
                 }
             }
-            answerBox.setText("");
+            answerBox.setText(""); //clear text in the answer box
         });
 
-
+        //load images and set bounds
         for(int i = 0; i < images.length; i++) {
             images[i] = new JLabel(new ImageIcon(importImage("images/snowman_" + (i + 1) + ".png")));
             images[i].setBounds(0, 0, getWidth(), getHeight() - spaceForElements);
         }
 
+        //configure animation panel
         animationPanel.setBackground(getBackgroundColor());
         animationPanel.setBounds(0, 0, getWidth(), getHeight() - spaceForElements);
         animationPanel.add(images[wrongAnswers]);
         animationPanel.setLayout(null);
-
-
-
-
+        
+        //set layout and add components to Playscreen
         setLayout(null);
         add(answerBox);
         add(problemField);
         add(guess);
         add(animationPanel);
 
+        //add snowflakes to the animation panel
         for(int i = 0; i < snowflakes.length; i++) {
             snowflakes[i] = new SnowFlake(animationPanel);
             animationPanel.add(snowflakes[i]);
@@ -129,28 +141,37 @@ public class PlayScreen extends GamePanel{
 
     }
 
+    // problems generated based on difficulty
     public void generateProblems(String difficulty) {
-        switch (difficulty) {
+        switch (difficulty) { //switch to handle each difficulty level
             case "Easy": {
-                for (int i = 0; i < problems.length; i++) {
+                //for each index in the problems array
+                for (int i = 0; i < problems.length; i++) { 
+                    //create new instance of easy problems
                     problems[i] = new EasyProblems();
                 }
             }
             break;
             case "Medium": {
+                //for each index in the problems array
                 for (int i = 0; i < problems.length; i++) {
+                    //create new instance of medium problems
                     problems[i] = new MediumProblems();
                 }
             }
             break;
             case "Hard": {
+                //for each index in the problems array
                 for (int i = 0; i < problems.length; i++) {
+                    //create new instance of hard problems
                     problems[i] = new HardProblems();
                 }
             }
             break;
             case "Insanity": {
+                //for each index in the problems array
                 for (int i = 0; i < problems.length; i++)
+                    //create new instance of insane problems
                     problems[i] = new InsanityProblems();
             }
         }
