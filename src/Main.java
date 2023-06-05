af90b6ab3be17b9e8d8cb61652fadcb03de50f3b
Thomas Huynh <81395779+Thomas-Huynh@users.noreply.github.com>
2023-06-04 21:44:58 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,6 @@ public class Main {
         JFrame frame = new JFrame(); // create a new JFrame, which allows us to create the UI
 
         frame.setSize(500, 500); // set the size to 500x500 pixels
-        frame.setLayout(null); // null layout allows for absolute positioning, allows us to manually edit the UI
         frame.setResizable(false); // focused on absolute positioning to stylize so kept to false
         frame.setTitle("Melting Snowman Game"); // set title of panel
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // on close, exit
