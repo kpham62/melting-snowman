269581baa30ca5d8fffc5533e6cc5bfb8dca1eae
Thomas Huynh <81395779+Thomas-Huynh@users.noreply.github.com>
2023-06-04 21:46:34 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,4 @@
 import javax.swing.*;
-import javax.imageio.ImageIO;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.Image;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.Timer;
-import java.util.Objects;
-import javax.imageio.*;
 
 public class Main {
     public static void main(String[] args) {
