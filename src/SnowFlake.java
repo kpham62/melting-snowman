e18caecc2ae5142eedc7bc75de4d57cdaf5489fb
Thomas Huynh <81395779+Thomas-Huynh@users.noreply.github.com>
2023-06-04 21:49:15 -0700

    Update SnowFlake.java

--- src/SnowFlake.java
+++ src/SnowFlake.java
@@ -4,7 +4,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
 import java.util.Objects;
 
 //class defined extending to JLabel
