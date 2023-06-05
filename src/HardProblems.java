08e2db8443196344cbfa4c7369c67c4655f3c560
Thomas Huynh <81395779+Thomas-Huynh@users.noreply.github.com>
2023-06-04 19:36:17 -0700

    Update HardProblems.java

--- src/HardProblems.java
+++ src/HardProblems.java
@@ -1,38 +1,43 @@
 public class HardProblems extends Problems{
-    private static int num1;
-    private static int num2;
     public HardProblems() {
         int problemTypeGenerator = (int)(Math.random() * 3);
 
         switch(problemTypeGenerator) {
-            case 0:
-                num1 = (int)(Math.random() * 51) + 50;
+            case 0: {
+                int num1 = (int)(Math.random() *  50) + 50;
 
-                setQuestion("the sum of the first " + num1 + " numbers");
+                setQuestion("1 + 2 + 3 + ... " + " + " + (num1 - 1 ) + " + " + num1);
                 setAnswer((num1) * (num1 + 1) / 2);
+                setProblemSize(200);
+            }
                 break;
-            case 1:
-                num1 = (int)(Math.random() * 5) + 5;
-                num2 = (int)(Math.random() * 3) + 3;
+            case 1: {
+                int num1 = (int) (Math.random() * 5) + 5;
+                int num2 = (int) (Math.random() * 3) + 3;
 
                 setQuestion(num1 + " ^ " + num2);
                 setAnswer(Math.pow(num1, num2));
+                setProblemSize(100);
+            }
                 break;
-            case 2:
-                num1 = (int)(Math.random() * 10);
+
+            case 2: {
+                int num1 = (int)(Math.random() * 6) + 5;
 
                 if (num1 == 0) {
                     setAnswer(1);
-                }
-                else{
+                } else {
                     int answer = 1;
-                    for(int i = 1; i <= num1; i++) {
+                    for (int i = 1; i <= num1; i++) {
                         answer *= i;
                     }
                     setAnswer(answer);
 
                 }
-                setQuestion(num1 + "!");
+                setQuestion(num1 + "! ");
+                setProblemSize(100);
+            }
+                break;
 
         }
     }
